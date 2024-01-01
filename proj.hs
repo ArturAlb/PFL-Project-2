1cc8403baaa62737e5234f61329bd397fd0009fb
LeM0n17 <93678059+LeM0n17@users.noreply.github.com>
2024-01-01 21:48:48 +0000

    sdngibdfnvjbdfj

--- proj.hs
+++ proj.hs
@@ -14,7 +14,7 @@ import Parser
 -- Do not modify our definition of Inst and Code
 data Inst =
     Push Integer | Add | Mult | Sub | Tru | Fals | Equ | Le | And | Neg | Fetch String | Store String | Noop |
-    Branch Code Code | Loop Code Code
+    Branch Code Code | Loop Code Code deriving Show
 type Code = [Inst]
 tt :: Bool
 tt = True
@@ -91,6 +91,7 @@ run ( Branch c1 c2 :instructions, stack, state) = if topBool stack == tt then ru
 run ( Loop c1 c2 :instructions, stack, state) = run (loop (c1,c2) ++ instructions,stack,state)
 run ( Noop :instructions, stack, state) = run (instructions, stack, state)
 run ( Neg :instructions, stack, state) = run (instructions, push (BoolElem (not (topBool stack))) (pop stack), state)
+run ( And :instructions, stack, state) = run (instructions, push (BoolElem (topBool stack && topBool (pop stack))) (pop (pop stack)), state)
 run _ = error "Run-time error"
 
 
@@ -134,13 +135,13 @@ parseCode str = case words str of
 compA :: Aexp -> Code
 compA (VarExp x) = [Fetch x]
 compA (I n) = [Push n]
-compA (AddExp aexp1 aexp2) = (compA aexp1) ++ (compA aexp2) ++ [Add]
-compA (SubExp aexp1 aexp2) = (compA aexp2) ++ (compA aexp1) ++ [Sub]
+compA (AddExp aexp1 aexp2) = compA aexp1 ++ compA aexp2 ++ [Add]
+compA (SubExp aexp1 aexp2) = compA aexp2 ++ compA aexp1 ++ [Sub]
 compA (MultExp aexp1 aexp2) = compA aexp1 ++ compA aexp2 ++ [Mult]
 
 compB :: Bexp -> Code
-compB (BTrue) = [Tru]
-compB (BFalse) = [Fals]
+compB BTrue = [Tru]
+compB BFalse = [Fals]
 compB (IEqExp aexp1 aexp2) = compA aexp1 ++ compA aexp2 ++ [Equ]
 compB (EqExp bexp1 bexp2) = compB bexp1 ++ compB bexp2 ++ [Equ]
 compB (LeExp aexp1 aexp2) = compA aexp1 ++ compA aexp2 ++ [Le]
@@ -149,9 +150,9 @@ compB (AndExp bexp1 bexp2) = compB bexp1 ++ compB bexp2 ++ [And]
 
 compile :: Program -> Code
 compile [] = []
-compile (Assign var aexp : rest) = (compA aexp) ++ [Store var] ++ compile rest
-compile (If bexp p1 p2 : rest) = (compB bexp) ++ [Branch (compile p2) (compile p1)] ++ compile rest
-compile (While bexp p : rest) = (compB bexp) ++ [Loop (compile p) (compB bexp)] ++ compile rest
+compile (Assign var aexp : rest) = compA aexp ++ [Store var] ++ compile rest
+compile (If bexp p1 p2 : rest) = compB bexp ++ [Branch (compile p1) (compile p2)] ++ compile rest
+compile (While bexp p : rest) = Loop (compB bexp) (compile p) : compile rest
 
 parse :: String -> Program
 parse text = extractStm (parseStms (lexer text))
@@ -180,10 +181,10 @@ testParser programCode = (stack2Str stack, state2Str state)
 -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
 main :: IO ()
-main = do 
+main = do
     print( testParser "x := 0 - 2;" == ("","x=-2"))
     print( testParser "x := 5; x := x - 1;" == ("","x=4"))
-    --print( testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2"))
+    print( testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2"))
     print( testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1"))
     print( testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2"))
     print( testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4"))
@@ -192,4 +193,4 @@ main = do
     print( testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1"))
     print( testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2"))
     print( testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6"))
-    print( testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1"))
+    print ( testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1"))
